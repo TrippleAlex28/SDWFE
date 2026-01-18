161e378b8822ce4479af59a0cf3eb820842a3c01
Tijn Reijke <tijnreijke@hotmail.com>
2026-01-18 16:37:26 +0100

    quick fix

--- SDWFE/src/Objects/Tilemap/TilemapData.cs
+++ SDWFE/src/Objects/Tilemap/TilemapData.cs
@@ -48,7 +48,7 @@ public static class LevelNames
     {
         { -1, "Hub" },
         { 0, "Level1" },
-        { 1, "Level1" },
+        { 1, "Level2" },
         { 2, "Level3" },
         { 3, "Level4" },
         { 4, "Level5" }
